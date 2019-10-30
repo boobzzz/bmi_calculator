5faa17eb8c3a62b1342a8a475851f63c99ddfbea
Yuriy Borys <iam618384@gmail.com>
2019-10-30 23:41:45 +0200

    Initial commit

---
+++ src/components/Range/Range.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Range = (props) => (
+    <div>
+        <label>
+            <input type="range"/>
+            <span>cm</span>
+        </label>
+    </div>
+)
+
+export default Range;
